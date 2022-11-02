c801734d3aff60bb6b79123f72fd506854f84c96
ryansldev <ryanlima1309@gmail.com>
2022-11-02 15:51:06 -0300

    feat: add sign in screen

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react"
+import { Center, Text } from "native-base"
+
+export const SignIn: FC = () => {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24}>
+        SignIn
+      </Text>
+    </Center>
+  ) 
+}
